491e1a3e09bfd2d29de24c245e6c4ff5d9d5beaa
dzer6 <andrew@dzer6.com>
2024-04-02 00:20:32 +0200

    Initial commit

--- src/main/clj/calc/rpc/controller/calculation.clj
+++ src/main/clj/calc/rpc/controller/calculation.clj
@@ -54,5 +54,5 @@
       (log/error e)
       (error/fire :error-type :resource/bad-request
                   :cause e
-                  :message "Unable to find expressions"))))
+                  :message "Unable to obtain past evaluations."))))
 
